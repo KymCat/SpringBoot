90eea1b00d799dde0ba053f4a14b7c7d83e58c3c
KymCat <kohina1452@gmail.com>
2025-02-18 08:16:15 +0900

    MVC패턴 : bye controller, view template 추가

--- src/main/java/com/example/springBootProject/controller/FirstController.java
+++ src/main/java/com/example/springBootProject/controller/FirstController.java
@@ -1,14 +1,24 @@
 package com.example.springBootProject.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class FirstController {
 
     @GetMapping("/hi") // localhost:8080/hi 접속 -> greetings.mustache 파일을 찾아 반환
-    public String niceToMeetYou() {
+    public String niceToMeetYou(Model model) {
+
+        // model 객체가 "유민" 값을 "username"에 연결해 웹 브라우저로 보냄
+        model.addAttribute("username", "유민");
         return "greetings"; // greetings.mustache 파일 반환
 
+    }
+
+    @GetMapping("/bye")
+    public String seeYouNext(Model model) {
+        model.addAttribute("nickname", "유민");
+        return "goodbye";
     }
 }
