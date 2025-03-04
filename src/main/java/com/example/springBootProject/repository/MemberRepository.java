b8047f3a5570c30be68a2a3a7538b37355a10784
KymCat <kohina1452@gmail.com>
2025-03-04 15:09:14 +0900

    add : 회원가입 리다이렉트 설정

--- src/main/java/com/example/springBootProject/repository/MemberRepository.java
+++ src/main/java/com/example/springBootProject/repository/MemberRepository.java
@@ -3,5 +3,9 @@ package com.example.springBootProject.repository;
 import com.example.springBootProject.entity.Member;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.ArrayList;
+
 public interface MemberRepository extends CrudRepository<Member, Long> {
+    @Override
+    ArrayList<Member> findAll();
 }
