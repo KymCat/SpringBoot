fd33464857d84fce193c083792da253e2e48b661
KymCat <kohina1452@gmail.com>
2025-03-04 16:23:46 +0900

    add : 게시글 수정 기능

--- src/main/java/com/example/springBootProject/dto/ArticleForm.java
+++ src/main/java/com/example/springBootProject/dto/ArticleForm.java
@@ -7,11 +7,12 @@ import lombok.ToString;
 @AllArgsConstructor // 생성자 자동생성
 @ToString // ToString() 메서드 자동생성
 public class ArticleForm {
+    private Long id;
     private String title; // 제목 받을 필드
     private String content; // 내용을 받을 필드
 
 
     public Article toEntity() {
-        return new Article(null, title, content);
+        return new Article(id, title, content);
     }
 }
